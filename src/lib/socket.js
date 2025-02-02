4a7054ec075e6298bbcf3a140ff2f99e10a7b345
Harshit <harshit2001pandey@gmail.com>
2025-02-02 21:48:05 +0530

    Added Socket for online users

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -12,10 +12,23 @@ const io = new Server(server, {
     },
 });
 
+
+const userSocketMap = {};
+
+
 io.on("connection", (socket) => {
     console.log("User connected", socket.id);
-    socket.on("disconnect", ()=>{
-        console.log("User disconnected", socket.id);
-    })
-})
+    const userId = socket.handshake.query.userId;
+    if(userId){
+        userSocketMap[userId] = socket.id;
+    }
+    // io.emit is used to send events to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", () => {
+        console.log("A user disconnected", socket.id);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    });
+});
 export {io, app, server};
