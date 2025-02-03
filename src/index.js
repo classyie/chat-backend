cf0f6434d45d7ec2473827b1c089d71571c91f90
Harshit <harshit2001pandey@gmail.com>
2025-02-03 22:29:11 +0530

    project Completed

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import cookieParser from "cookie-parser";
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import dotenv from 'dotenv';
+import path from 'path';
 import cors from 'cors';
 import { connectDB } from './lib/db.js';
 import {app, server} from './lib/socket.js';
 dotenv.config();
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // const app = express()
 app.use(express.json());
@@ -20,6 +22,12 @@ app.use(cors ({
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
 server.listen(PORT, ()=>{
     console.log('Server is running on port 5001');
     connectDB();
